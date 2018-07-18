57638b1e25e5bee6269ca522691e52c2414afd45
Elias Santistevan <elias.santistevan@sparkfun.local>
2018-07-18 12:37:46 -0600

    Some comment adjustments to all example/firmware code

--- Examples/Example1_ReadTag/Example1_ReadTag.ino
+++ Examples/Example1_ReadTag/Example1_ReadTag.ino
@@ -1,5 +1,4 @@
 /*
-
 	Qwiic RFID board fot the ID-XXLA Modules
 	By: Elias Santistevan
 	Sparkfun Electronics
@@ -9,31 +8,35 @@
 	Feel like supporting our work? Buy a board from Sparkfun!
 	<website here>
 
-	This example will do one thing for you: It will give the RFID tag's ID of the last tag scanned. 
-
+	This example will do one thing for you: It will give the RFID tag's ID of the first tag scanned. 
 */
+
 #include <Wire.h> 
 
 #define RFID_ADDR 0x7D //0x7D is the default address, 0x7C if you close the address jumper.
 
-byte tagID[7]; //Length of the tag is 5 bytes long and 1 byte of checksum.
-const int eventPin = 8; //We'll use a pin to initiate the check for the RFID tag ID. Feel free to remove it. 
+byte tagID[7]; //Length of the tag is 5 bytes long followed by 1 byte of checksum.
+
+//We'll use a pin attached to the interrupt line to initiate the check for the RFID tag ID. 
+//Feel free to remove it and do a manual check.
+const int eventPin = 8;  
 
 void setup()
 {
 	Wire.begin(); 
 	Serial.begin(9600); 
 	Serial.println("Waiting for interrupt to fire which indicates a tag has been read!"); 
-	pinMode(eventPin, INPUT_PULLUP); 
+	pinMode(eventPin, INPUT_PULLUP);//Our pin is active low so let's put it in a known high state.
 }
 void loop()
 {
 	if( digitalRead(eventPin) == LOW ) checkTagID();
 	delay(250);//slow it down
-	//If the tag reads 00000, then there are no new tags waiting to be read. The interrupt pin
-	//will not go low if there aren't new tags to be read as well. 
 }
 
+//20 tags can be stored by the product at a time, the first one to be scanned is the first one
+//to be pulled from the tag stack. If the tag reads '000000' or the interrupt line never went low
+//then there are no new tags waiting to be read. 
 void checkTagID()
 {	
 	Serial.print("RFID Tag ID: "); 
